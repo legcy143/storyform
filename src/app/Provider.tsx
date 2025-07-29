a7a43bf87e5aacab3921df864223ccf501b46294
prince <legcy143@gmail.com>
2025-07-29 22:16:11 +0530

    basic: dynamic form structure

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -7,7 +7,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
             <div className='bg-primary absolute -top-[5rem] -left-[5rem] size-[23rem] blur-[15rem]' />
             <div className='bg-primary absolute -bottom-[5rem] -right-[5rem] size-[23rem] blur-[15rem]' />
             <h1 className='text-2xl font-bold'>Story Form</h1>
-            <section>
+            <section className='w-full max-w-[40rem] mx-auto '>
                 {children}
             </section>
             <footer className='mt-8 text-sm text-gray-500'>
--- src/app/_pages/paragraphForm/index.tsx
+++ src/app/_pages/paragraphForm/index.tsx
@@ -1,11 +1,35 @@
+import ThemeButton from '@/components/ui/ThemeButton'
 import { Divider } from '@heroui/react'
 import React from 'react'
 
 export default function ParagraphForm() {
+  const word = "At %confrence% , %Alex% , a %Role% at %company% , unveiled their latest concept: %invention% ."
+
+
   return (
-    <section className='animate-appearance-in'>
-      <h1>The Big Reveal</h1>
+    <section className='animate-appearance-in border border-gray-700/50 rounded-2xl p-5 bg-black/50 backdrop-blur-sm'>
+      <h1 className='text-center text-2xl md:text-3xl font-semibold' >The Big Reveal</h1>
       <Divider />
+      <div className='flex flex-row flex-wrap gap-2 capitalize font-semibold text-center transition-all'>
+        {
+          word?.split(" ")?.map((e, i) => {
+            console.log("e => ", e)
+            if (e.startsWith("%")) {
+              return <input
+                key={i}
+                placeholder={e.replaceAll("%", "")}
+                className='field-sizing-content min-w-[5rem] max-w-full outline-none border-b border-primary text-center text-primary placeholder:text-gray-400 px-1 transition-all'
+              />
+            }
+            return <p key={i} className='transition-all'>
+              {e}
+            </p>
+          })
+        }
+      </div>
+
+      <ThemeButton>Continue</ThemeButton>
+
     </section>
   )
 }
