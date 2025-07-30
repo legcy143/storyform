40ee20eb2bc7447f3fe71fa6e0b9192a5a7c8f55
prince <legcy143@gmail.com>
2025-07-30 21:00:44 +0530

    feat: add camera instruction and update themebutton

--- src/app/_pages/cameraInstruction/index.tsx
+++ src/app/_pages/cameraInstruction/index.tsx
@@ -1,7 +1,38 @@
+import ThemeButton from '@/components/ui/ThemeButton'
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import React from 'react'
+import { LuCircleCheck, LuCircleX, LuInfo } from 'react-icons/lu';
+import usePagging from '@/app/_store/usePagging';
 
 export default function CameraInstruction() {
+  const setCurrentPage = usePagging(state => state.setCurrentPage);
+  const GoPrevious = () => {
+    setCurrentPage("paragraphForm")
+  }
+
+  const handleNext = () => {
+    setCurrentPage("disclaimer")
+  }
   return (
-    <div>CameraInstruction</div>
+    <section className='animate-appearance-in border border-gray-400/50 rounded-2xl p-5 bg-gray-500/50 backdrop-blur-sm  flex flex-col gap-7 w-fit m-auto'>
+      <h1 className='text-center text-xl md:text-3xl font-bold'>Take a clear , well-lit photo of yourself</h1>
+      <div className='flex flex-col md:flex-row gap-5 items-center justify-center'>
+        <img
+          className='w-full max-w-[15rem] rounded-lg'
+          src="https://gkh-images.s3.amazonaws.com/ca9c9462-c591-426e-86dd-578d4ca7cd62_alex-suprun-ZHvM3XIOHoE-unsplash.jpg" alt="" />
+        <div className='text-lg md:text-xl font-normal flex flex-col gap-3 md:gap-5 justify-between '>
+          <h1 className='flex items-center gap-2'> <LuCircleCheck className='text-green-500' /> Good Lighting and Quality </h1>
+          <h1 className='flex items-center gap-2'> <LuCircleCheck className='text-green-500' /> Center your face in the frame </h1>
+          <h1 className='flex items-center gap-2'> <LuCircleX className='text-red-500' /> No Accesosories or other people </h1>
+          <h1 className='flex items-center gap-2'> <LuInfo className='text-yellow-500' /> Ai ins't perfect. not quite right ? <br />Give it another go  </h1>
+        </div>
+      </div>
+      <div className='flex flex-row justify-between items-center'>
+        <ThemeButton isIconOnly={true} onPress={GoPrevious}>
+          <MdOutlineKeyboardArrowLeft className='size-7' />
+        </ThemeButton>
+        <ThemeButton onPress={handleNext}>Next</ThemeButton>
+      </div>
+    </section>
   )
 }
--- src/components/ui/ThemeButton.tsx
+++ src/components/ui/ThemeButton.tsx
@@ -1,9 +1,11 @@
 import { Button, ButtonProps, cn } from '@heroui/react'
 import React from 'react'
 
-export default function ThemeButton({ children, className, ...props }: { children: React.ReactNode, className?: string } & ButtonProps) {
+export default function ThemeButton({ children, className, isIconOnly, ...props }: { children: React.ReactNode, className?: string, isIconOnly?: boolean } & ButtonProps) {
   return (
-    <Button color='primary' {...props} className={cn("text-md md:text-lg p-7 md:p-9  py-5 md:py-6 rounded-full font-semibold",className)} >
+    <Button color='primary' isIconOnly={isIconOnly} {...props} className={cn(
+      isIconOnly ? "rounded-full p-2 scale-100 md:scale-125" : "text-md md:text-lg p-7 md:p-9  py-5 md:py-6 rounded-full font-semibold"
+      , className)} >
       {children}
     </Button>
   )
