1ba60fd40789add789fece3c1fc3710ffd2470a3
prince <legcy143@gmail.com>
2025-07-31 22:17:30 +0530

    feat: handle process image

--- src/app/download/[taskId]/page.tsx
+++ src/app/download/[taskId]/page.tsx
@@ -19,32 +19,32 @@ export default function page() {
         }
     }, [])
 
- const handleDownload = async () => {
-    setIsLoading(true);
-    try {
-        if (!portrait?.resultImage) {
+    const handleDownload = async () => {
+        setIsLoading(true);
+        try {
+            if (!portrait?.resultImage) {
+                setIsLoading(false);
+                return;
+            }
+
+            const response = await fetch(portrait.resultImage, { mode: 'cors' });
+            const blob = await response.blob();
+            const url = window.URL.createObjectURL(blob);
+
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `portrait_${taskId}.png`;
+            document.body.appendChild(link);
+            link.click();
+
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
+        } catch (err) {
+            console.error('Download failed:', err);
+        } finally {
             setIsLoading(false);
-            return;
         }
-
-        const response = await fetch(portrait.resultImage, { mode: 'cors' });
-        const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = `portrait_${taskId}.png`;
-        document.body.appendChild(link);
-        link.click();
-
-        document.body.removeChild(link);
-        window.URL.revokeObjectURL(url);
-    } catch (err) {
-        console.error('Download failed:', err);
-    } finally {
-        setIsLoading(false);
-    }
-};
+    };
 
 
 
@@ -66,9 +66,18 @@ export default function page() {
 
     return (
         <section className='animate-appearance-in border border-gray-400/50 rounded-2xl p-5 bg-gray-500/50 backdrop-blur-sm w-fit  flex flex-col gap-7 m-auto'>
-            <Img src={portrait.resultImage} alt="result" className='max-h-[20rem] object-contain rounded-xl bg-red-500 w-fit' />
+            {
+                portrait.resultImage ? (
+                    <Img src={portrait.resultImage} alt="result" className='max-h-[20rem] object-contain rounded-xl bg-red-500 w-fit' />
+                ) : (
+                    <div>
+                        <object data={"/loading.gif"} type="image/gif"></object>
+                        <p>Processing your image...</p>
+                    </div>
+                )
+            }
             <div className='flex items-center justify-center gap-1'>
-                <Button onPress={handleDownload} isLoading={isLoading}><LuDownload /> Download</Button>
+                <Button onPress={handleDownload} isDisabled={portrait.status !== 'completed'} isLoading={isLoading}><LuDownload /> Download</Button>
             </div>
         </section>
     )
